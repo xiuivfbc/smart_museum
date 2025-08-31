2838461c46da9dd67a90b6c247ae8009d3c98593
mc <hvfwdemc@qq.com>
2025-08-31 16:46:13 +0800

    少提交了一个，淦

--- model/user.go
+++ model/user.go
@@ -5,12 +5,12 @@ package model
 import "time"
 
 type User struct {
-	ID        int       `json:"id" gorm:"primaryKey"`      // 用户唯一ID
-	Username  string    `json:"username" `                 // 用户名
-	Password  string    `json:"password"`                  // 密码（加密存储）
-	Role      string    `json:"role"`                      // 角色（如admin/user等）
-	Phone     string    `json:"phone" "gorm:"uniqueIndex"` // 电话
-	Email     string    `json:"email" "gorm:"uniqueIndex"` // 邮箱
-	CreatedAt time.Time `json:"created_at"`                // 创建时间
-	UpdatedAt time.Time `json:"updated_at"`                // 更新时间
+	ID        int       `json:"id" gorm:"primaryKey"`                      // 用户唯一ID
+	Username  string    `json:"username" `                                 // 用户名
+	Password  string    `json:"password"`                                  // 密码（加密存储）
+	Role      string    `json:"role"`                                      // 角色（如admin/user等）
+	Phone     string    `json:"phone" gorm:"type:varchar(32);uniqueIndex"` // 电话
+	Email     string    `json:"email" gorm:"type:varchar(32);uniqueIndex"` // 邮箱
+	CreatedAt time.Time `json:"created_at"`                                // 创建时间
+	UpdatedAt time.Time `json:"updated_at"`                                // 更新时间
 }
